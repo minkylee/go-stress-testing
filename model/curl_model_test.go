25059fb3da0f827a53179ee20d8cc16122dee615
mingqi.li <mingqi.li@shopee.com>
2021-06-07 17:16:43 +0800

    增加不同的client使用不同的curl的功能

---
+++ model/curl_model_test.go
@@ -0,0 +1,25 @@
+// Package model 数据模型
+package model
+
+import (
+	"testing"
+)
+
+// TestCurl 测试函数
+func TestCurl(t *testing.T) {
+	// ../curl.txt
+	//curls, err := ParseTheFile("/test.txt")
+	//
+	//if err != nil {
+	//	return
+	//}
+	//for i, c := range curls {
+	//	fmt.Printf("\n---------request start %d---------\n", i)
+	//	fmt.Printf("url:%s \n", c.GetURL())
+	//	fmt.Printf("method:%s \n", c.GetMethod())
+	//	fmt.Printf("body:%v \n", c.GetBody())
+	//	fmt.Printf("body string:%v \n", c.GetBody())
+	//	fmt.Printf("headers:%s \n", c.GetHeadersStr())
+	//	fmt.Printf("---------request end %d---------\n\n", i)
+	//}
+}
